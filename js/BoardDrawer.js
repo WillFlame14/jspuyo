054b6d193b1400689b1d77f76f6847cb2e827b70
Kieran Quan <k4quan@edu.uwaterloo.ca>
2020-02-25 21:33:04 -0500

    improve puyo appearance with drawings

--- js/BoardDrawer.js
+++ js/BoardDrawer.js
@@ -1,18 +1,29 @@
-let c = document.getElementById("mainCanvas");
+let c = document.getElementById("board");
 let ctx = c.getContext("2d");
+const PUYO_RED = "rgba(200, 20, 20, 0.9)";
+const PUYO_BLUE = "rgba(20, 20, 200, 0.9)";
+const PUYO_GREEN = "rgba(20, 200, 20, 0.9)";
+const PUYO_EYES = "rgba(255, 255, 255, 0.7)";
 
-ctx.fillRect(25, 250, 25, 25);
+alert("begin script")
 
 function fillBlock(xPos, yPos, colour) {
+    ctx.beginPath();
     ctx.fillStyle = colour;
-    ctx.fillRect(xPos * 25, 300 - (yPos + 1) * 25, 25, 25);
-}
-fillBlock(0, 1, "red");
-fillBlock(0, 2, "red");
-fillBlock(1, 1, "red");
-fillBlock(0, 0, "blue");
-fillBlock(1, 0, "blue");
-fillBlock(2, 1, "blue");
-fillBlock(1, 2, "blue");
-fillBlock(2, 0, "green");
+    ctx.arc(xPos * 25 + 25 / 2, 300 - (yPos + 0.5) * 25, 25 / 2, 0, 2 * Math.PI);
+    ctx.fill();
 
+    ctx.beginPath();
+    ctx.fillStyle = PUYO_EYES;
+    ctx.arc(xPos * 25 + 25 / 2 - 5, 300 - (yPos + 0.5) * 25 - 3, 5, 0, 2 * Math.PI);
+    ctx.arc(xPos * 25 + 25 / 2 + 5, 300 - (yPos + 0.5) * 25 - 3, 5, 0, 2 * Math.PI);
+    ctx.fill();
+}
+fillBlock(0, 1, PUYO_RED);
+fillBlock(0, 2, PUYO_RED);
+fillBlock(1, 1, PUYO_RED);
+fillBlock(0, 0, PUYO_BLUE);
+fillBlock(1, 0, PUYO_BLUE);
+fillBlock(2, 1, PUYO_BLUE);
+fillBlock(1, 2, PUYO_BLUE);
+fillBlock(2, 0, PUYO_GREEN);

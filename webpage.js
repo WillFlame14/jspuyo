e99690a7e4a35b39473e5eb6b78a08841a338d6f
WillFlame <flameyanai@gmail.com>
2020-04-26 13:33:58 -0400

    Navbar no longer pushes down content
    Transitions are much smoother.
    Added comments.

--- webpage.js
+++ webpage.js
@@ -1,25 +1,29 @@
 'use strict';
 
-let currentlyExpanded = null;
-const heights = {
+let currentlyExpanded = null;	// The currently expanded panel's id
+const dropdownItems = {			// Number of items in each dropdown menu
 	"queuePanel": 2,
 	"customPanel": 3,
 	"singleplayerPanel": 3,
 	"profilePanel": 2
 }
 
+/**
+ * Expands a dropdown menu and closes any other open dropdown menu.
+ * If the current dropdown menu is already open, it is closed.
+ */
 function expand_dropdown(id) {
 	if(currentlyExpanded === id) {
 		document.getElementById(id).classList.remove('expanded');
-		document.getElementById(id).style.height = "81";
+		document.getElementById(id).querySelector('.dropdown').style.height = "0";
 		currentlyExpanded = null;
 	}
 	else {
-		document.getElementById(id).style.height = `${80 + heights[id] * 40}`;
+		document.getElementById(id).querySelector('.dropdown').style.height = `${dropdownItems[id] * 40}`;
 		document.getElementById(id).classList.add('expanded');
 		if(currentlyExpanded !== null) {
 			document.getElementById(currentlyExpanded).classList.remove('expanded');
-			document.getElementById(currentlyExpanded).style.height = "81";
+			document.getElementById(currentlyExpanded).querySelector('.dropdown').style.height = "0";
 		}
 		currentlyExpanded = id;
 	}

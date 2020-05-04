19ab0883672b4b00abc1fc14c1f84200dbac8956
Kieran Quan <k4quan@edu.uwaterloo.ca>
2020-05-04 01:07:01 -0400

    fix drawSprite for proper scaling, add framework for drawSubsprite

--- js/Draw.js
+++ js/Draw.js
@@ -4,6 +4,8 @@ const SHEET_ROWS = 16;  // number of rows in the sprite grid
 const SHEET_COLS = 16;  // number of columns in the sprite grid
 const SHEET_UNIT = 32;  // number of pixels in a sprite grid unit
 const SHEET_GAP = 1;    // number of pixels before sprite starts (top/left)
+const SHEET_USED_UNIT = SHEET_UNIT - SHEET_GAP;
+const SUB_SCALE_FACTOR = 1.025;
 
 /**
  * Stores and loads scaled sprites.
@@ -17,13 +19,18 @@ class SpriteDrawer {
             const canvasName = 'c' + size.toString();
             ctx.drawImage(
                 this[spriteSheet][canvasName],
-                (sX * (SHEET_UNIT + SHEET_GAP) / SHEET_UNIT + SHEET_GAP / SHEET_UNIT) * size,
-                (sY * (SHEET_UNIT + SHEET_GAP) / SHEET_UNIT + SHEET_GAP / SHEET_UNIT) * size,
-                sWidth * size, sHeight * size,
+                (sX * SHEET_UNIT / SHEET_USED_UNIT + 1 / SHEET_USED_UNIT) * size,
+                (sY * SHEET_UNIT / SHEET_USED_UNIT + 1 / SHEET_USED_UNIT) * size,
+                sWidth * size + (sWidth - 1) * size / SHEET_USED_UNIT,
+                sHeight * size + (sHeight - 1) * size / SHEET_USED_UNIT,
                 cX * size - sWidth * size / 2, cY * size - sHeight * size / 2,
-                sWidth * size, sHeight * size
+                sWidth * size + (sWidth - 1) * size / SHEET_USED_UNIT,
+                sHeight * size + (sHeight - 1) * size / SHEET_USED_UNIT
             )
         }
+    }
+    drawSubsprite(ctx, spriteSheet, size, sX, sY, cX, cY, sWidth = 1, sHeight = 1) {
+
     }
     // Loads canvas with scaled sprite sheet if it hasn't been done yet
     // Will return false if the original image hasn't been loaded and cannot be accessed to scale into a canvas
@@ -37,8 +44,8 @@ class SpriteDrawer {
             // e.g. this['aqua']['c50'] or this.aqua.c50 is a canvas that has aqua.png drawn with unit size 50
             if(this[spriteSheet][canvasName] == null) {
                 this[spriteSheet][canvasName] = document.createElement('canvas');
-                this[spriteSheet][canvasName].width = size * SHEET_COLS * SHEET_UNIT / (SHEET_UNIT - SHEET_GAP);
-                this[spriteSheet][canvasName].height = size * SHEET_ROWS * SHEET_UNIT / (SHEET_UNIT - SHEET_GAP);
+                this[spriteSheet][canvasName].width = Math.ceil(SHEET_COLS * SHEET_UNIT * size / SHEET_USED_UNIT);
+                this[spriteSheet][canvasName].height = Math.ceil(SHEET_ROWS * SHEET_UNIT * size / SHEET_USED_UNIT);
                 this[spriteSheet][canvasName].getContext('2d').drawImage(
                     this[spriteSheet].image,
                     0, 0,
